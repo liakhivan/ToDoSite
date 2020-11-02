eb1e37a96858e7ffb9a547d4f68e5986d97a5a10
liakhivan <liakhivan21@gmail.com>
2020-11-02 21:32:02 +0200

    Added jquery and create request to change task status using ajax.

---
+++ ToDoSite.WebUI/Scripts/CheckCompleteTaskScript.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $(document).on("click", ".completeCheck", function (e) {
+        $.ajax({
+            type: "POST", url: "Task/ChangeStatus", data: { id: e.target.id }
+        });
+    });
+});
