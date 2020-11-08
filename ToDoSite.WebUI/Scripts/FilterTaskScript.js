a947b43376053cbd23faaa480d5139e8c9f64584
liakhivan <liakhivan21@gmail.com>
2020-11-08 21:50:29 +0200

    Was wrote method for writing styles on active filter.

--- ToDoSite.WebUI/Scripts/FilterTaskScript.js
+++ ToDoSite.WebUI/Scripts/FilterTaskScript.js
@@ -8,17 +8,31 @@ function addEventsOnFilters(statusMas) {
         const itemFilter = document.createElement('li');
 
         const itemFilterLink = document.createElement('a');
+        itemFilterLink.id = statusMas[i];
         itemFilterLink.href = '/Task/Index/' + statusMas[i];
         itemFilterLink.textContent = (statusMas[i]);
-        itemFilterLink.addEventListener('click', (e) => {
-            e.target.className = 'checked';
-        });
 
         itemFilter.appendChild(itemFilterLink);
         filters.appendChild(itemFilter);
     }
 }
 
+function markCurrentFilter(statusTaskMass) {
+   
+    let url = $(location).attr('href');
+
+    let segmentsUrlMass = url.split('/');
+
+    let isFilter = statusTaskMass.includes(segmentsUrlMass[segmentsUrlMass.length - 1]);
+
+    if (isFilter) {
+        document.getElementById(segmentsUrlMass[segmentsUrlMass.length - 1]).className += 'checked';
+    } else {
+        document.getElementById(statusTaskMass[0]).className = 'checked';
+    }
+  
+}
 
 const statusTaskMass = [ 'All', 'Active', 'Completed'];
 addEventsOnFilters(statusTaskMass);
+markCurrentFilter(statusTaskMass);
